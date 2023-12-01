3d5800ac4adba89ce115e0ff4179c910c357aa51
Harlow, Jordan <jordan.harlow@intel.com>
2023-12-01 14:09:49 -0700

    debug: run_all command type

--- bearysta/run.py
+++ bearysta/run.py
@@ -61,7 +61,7 @@ def run_benchmark(env, config, run_path='runs', run_id=None, commands=None,
         progress_inside = '{} ({}/{}) in {}'.format(endpoint, i+1, ncommands, progress)
         print('### Running benchmark endpoint', progress_inside)
 
-        if commands is not None and endpoint not in commands:
+        if commands is not None and 'run_all' not in commands and endpoint not in commands:
             print('[skipped this endpoint]')
             print(commands)
             print(endpoint)
